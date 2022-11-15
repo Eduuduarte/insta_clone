7a6dd93c14f58e051c74b65396d128567827c598
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-11-14 23:40:21 -0300

    creating the sidebar the feed

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import { faker } from '@faker-js/faker'
+import React, { useEffect, useState } from 'react'
 
 const Suggestions = () => {
+
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    setSuggestions([...Array(5)].map((profile) => ({
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      avatar: faker.image.avatar(),
+    }))
+    )
+  }, [])
+
   return (
-    <div>
-      
+    <div className='mt-4'>
+      <div className='flex items-center justify-between'>
+        <p className='text-sm font-semibold text-gray-400 mt-1'>Suggestion for you</p>
+        <button className='font-semibold text-xs'>See All</button>
+      </div>
+      {suggestions.map((profile) => (
+        <div className='flex items-center justify-between mt-5'>
+          <div className='flex items-center '>
+            <div className='w-8 h-8'>
+              <img src={profile.avatar} alt='avatar' className='rounded-full' />
+            </div>
+            <div className='ml-3'>
+              <p className='text-sm font-semibold'>{profile.username}</p>
+              <p className='text-xs text-gray-500'>Suggested for you</p>
+            </div>
+          </div>
+
+          <button className='text-xs font-semibold text-[#0096F6]'>Follow</button>
+        </div>
+      ))}
+
+      <div className='flex flex-wrap text-xs mt-6 text-semibold text-[#D2D2D2] space-x-1'>
+        <p>About</p>
+        <span>&#183;</span>
+        <p>Help</p>
+        <span>&#183;</span>
+        <p>Press</p>
+        <span>&#183;</span>
+        <p>API</p>
+        <span>&#183;</span>
+        <p>Jobs</p>
+        <span>&#183;</span>
+        <p>Privacy</p>
+        <span>&#183;</span>
+        <p>Terms</p>
+        <span>&#183;</span>
+        <p>Locations</p>
+        <span className='mt-1'>&#183;</span>
+        <p className='mt-1'>Lenguages</p>
+      </div>
+
+      <div className='text-xs mt-6 text-semibold text-[#D2D2D2]'>
+        <p>© 2022 INSTAGRAM FROM META</p>
+      </div>
     </div>
   )
 }
